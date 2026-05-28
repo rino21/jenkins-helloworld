ab859ea9b17ce455011e37905402b094c66b6728
rino21 <103988684+rino21@users.noreply.github.com>
2026-05-28 17:27:38 +0300

    ga

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
    	public static void main(String[] args){
-   		System.out.println("Hello RAMAMINIRINA Victorino");
+   		System.out.println("Hello Hairun Victorino");
    }
 }
